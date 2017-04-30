c4832e8460ee0d8170f07fa0e6e6408e9e10e8bf
RoadKillCat <joedominiciddon@gmail.com>
2017-04-30 17:17:36 +0100

    Update 3dx2.js

--- 3dx2.js
+++ 3dx2.js
@@ -28,7 +28,7 @@ window.addEventListener("deviceorientation", function(event){
    
    cam.x = angle
    //cam.y = tilt
-   cam.z = tilt *= -1
+   cam.z = tilt * -1
    
    document.getElementById("data").innerText = parseInt(angle) + "," + parseInt(tilt) + "," + parseInt(twist)
    
