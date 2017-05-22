5c362f53b928bccc7e0a0914f512719e5dc148a9
callum <callum.iddon@gmail.com>
2017-05-22 20:30:15 +0100

    Test2

--- createREADME.py
+++ createREADME.py
@@ -4,4 +4,4 @@ with open("README.md", "w") as fHandle:
     fHandle.write("# Links to the pages\n")
     for f in os.listdir():
         if ".html" in f:
-            fHandle.write("[{}](https://roadkillcat.github.io/3dSimulationVR/{})\n".format(f, f))
+            fHandle.write("- [{}](https://roadkillcat.github.io/3dSimulationVR/{})\n".format(f, f))
