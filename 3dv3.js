b32e0701abaf27efad5a54f6d0dbe86fa7a771d8
RoadKillCat <joedominiciddon@gmail.com>
2017-05-25 18:58:13 +0100

    Create 3dv3.js

--- 3dv3.js
+++ 3dv3.js
@@ -34,21 +34,28 @@ coordinates = [
 {x: 0, y: 16, z: 0}, {x: 0, y: 15, z: 0}, {x: -1, y: 15, z: 0}, {x: -1, y: 16, z: 0},					//square on floor
 {x: -4, y: 14, z: 0}, {x: -4, y: 23, z: 0}, {x: 4, y: 23, z: 0}, {x: 4, y: 14, z: 0},					//floor
 {x: -4, y: 23, z: 0}, {x: 4, y: 23, z: 0}, {x: 4, y: 23, z: 2.5}, {x: -4, y: 23, z: 2.5},			    //back wall
-{x: -1, y: 23, z: 1.8}, {x: 1, y: 23, z: 1.8}, {x: 0, y: 23, z: 0.3}									//triangle on back wall
+{x: -1, y: 23, z: 1.8}, {x: 1, y: 23, z: 1.8}, {x: 0, y: 23, z: 0.3},									//triangle on back wall
+{x: -10, y: 50, z: 0}, {x: -10, y: 60, z: 0}, {x: 10, y: 60, z: 0}, {x: 10, y: 50, z: 0}
+
+
+
 ]
 
 
 faceVerticies = [
+{v: [29,30,31,32], c: 5},
 {v: [22,23,24,25], c: 0}, 					//back wall
 {v: [26,27,28], c: 1},						//triangle on back wall
 {v: [18,19,20,21], c: 2},					//floor
 {v: [2,3,4,5], c: 3}, {v: [0,1,2,3], c: 3},																											//dad chair
 {v: [6,7,8,9], c: 4}, {v: [8,9,13,12], c: 4}, {v: [7,8,12,11], c: 4}, {v: [6,9,13,10], c: 4}, {v: [10,11,12,13], c: 4}, {v: [6,7,11,10], c: 4},		//tall collumn
-{v: [14,15,16,17], c: 5}]					//square on floor
-
+{v: [14,15,16,17], c: 5}					//square on floor
+]
 
 cam = {x: 0, y: 0, z: 3, pitch: 0, yaw: 0, roll: 0}		//coordinates of the camera
-pixAngleRatio = width / 50					//the amount of pixels that one degree spreads over
+fov = 50 						//field of view in degrees
+
+pixelsPerDegree = width / fov					//the amount of pixels that one degree spreads over
 step = 0.5
 
 distanceBetween = (co1, co2) => Math.sqrt(Math.pow(co2.x - co1.x , 2) + Math.pow(co2.y - co1.y , 2) + Math.pow(co2.z - co1.z , 2))
@@ -98,31 +105,69 @@ function drawPoints(canvasCoordinates){	//acctually does the drawing of the coor
 function renderObjects(){		//draws the 3d objects from their coordinates and cam position onto canvas in 2d	
 
 	coordinateAngles = coordinates.map(map3dTo2d)
-	canvasCoordinates = coordinateAngles.map(o => ( { x: width / 2 + (o.yaw * pixAngleRatio), y: height / 2 - (o.pitch * pixAngleRatio) } ))
+	canvasCoordinates = coordinateAngles.map(o => ( { x: width / 2 + (o.yaw * pixelsPerDegree), y: height / 2 - (o.pitch * pixelsPerDegree) } ))
 	
 	drawPoints(canvasCoordinates)
 }
 
 function map3dTo2d(coord){
-	yaw = degFromRad( Math.atan((coord.x - cam.x) / (coord.y - cam.y))  ) - cam.yaw
+	yaw = degFromRad( Math.atan((coord.x - cam.x) / (coord.y - cam.y)) ) - cam.yaw
 	pitch = degFromRad(Math.atan((coord.z - cam.z) / (coord.y - cam.y))) - cam.pitch
 	return {yaw: yaw, pitch: pitch}
 }
 
+	
 function renderMiniMap(){
 	mapWidth = width / 5
 	mapHeight = height / 5
 	border = 0.6
 	gap = 5
-	arrowLength = 30
+	arrowLength = 110
+	
 	ctx.fillStyle = "black"
 	ctx.fillRect(width - gap, gap, -mapWidth + border * -2, mapHeight + border * 2)
 	ctx.fillStyle = "white"
 	ctx.fillRect(width - border - gap, border + gap, -mapWidth, mapHeight)
-	ctx.moveTo(width - border - gap - mapWidth / 2, border + gap + mapHeight /2)
-	ctx.strokeStyle = "red"
-	ctx.lineTo(width - border - gap - mapWidth /2 +  (Math.sin(radFromDeg(cam.yaw)) * arrowLength), border + gap + mapHeight / 2 - (Math.cos(radFromDeg(cam.yaw)) * arrowLength))
-	ctx.stroke()
+	
+	centerX = width - border - gap - mapWidth / 2
+	centerY = border + gap + mapHeight / 2
+	scale = 2
+	pointLower = 70
+	
+	
+	for (f = 0; f < faceVerticies.length; f++){
+		face = faceVerticies[f]
+		shape = face.v
+		
+		ctx.strokeStyle = "black"
+		ctx.beginPath(centerX + coordinates[shape[0]].x * scale	, centerY  + coordinates[shape[0]].y * -1 * scale + pointLower)
+		for (p = 1; p < shape.length; p++){
+			ctx.lineTo(centerX + coordinates[shape[p]].x * scale, centerY  + coordinates[shape[p]].y * -1 * scale + pointLower)
+			ctx.stroke()
+		}
+		ctx.lineTo(centerX + coordinates[shape[0]].x * scale, centerY  + coordinates[shape[0]].y * -1 * scale + pointLower)
+		ctx.stroke()
+		ctx.closePath()
+		ctx.fillStyle = colors[face.c]
+		ctx.fill()
+	}
+	
+	drawLine(centerX - 5, centerY + pointLower, centerX + 5, centerY + pointLower)
+	drawLine(centerX, centerY - 5 + pointLower, centerX, centerY + 5 + pointLower)
+	
+	ctx.fillStyle = "#00ffed"
+	ctx.fillRect(centerX + cam.x -2 , centerY + cam.y * -1 - 2 + pointLower, 4, 4)
+	
+	
+	
+	ctx.fillStyle = "rgbA(107,255,125,0.3)"
+	ctx.beginPath(centerX + cam.x, centerY + cam.y * -1 + pointLower)
+	ctx.lineTo(centerX + (Math.sin(radFromDeg(cam.yaw - 0.5 * fov)) * arrowLength) + cam.x, centerY - (Math.cos(radFromDeg(cam.yaw - 0.5 * fov)) * arrowLength) + cam.y * -1 + pointLower)
+	ctx.arc(centerX + cam.x, centerY + cam.y * -1 + pointLower, arrowLength, radFromDeg(-90 - fov * 0.5 + cam.yaw), radFromDeg(-90 + fov * 0.5 + cam.yaw) )
+	//ctx.lineTo(centerX + (Math.sin(radFromDeg(cam.yaw + 0.5 * fov)) * arrowLength) + cam.x, centerY - (Math.cos(radFromDeg(cam.yaw + 0.5 * fov)) * arrowLength) + cam.y * -1 )
+	ctx.lineTo(centerX + cam.x, centerY + cam.y * -1 + pointLower)
+	ctx.closePath()
+	ctx.fill()
 	
 }
 
