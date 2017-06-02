d362b1201609484dc8f95fcc4cd9604daec0ad05
RoadKillCat <joedominiciddon@gmail.com>
2017-06-02 16:02:55 +0100

    Update 3dx2v3.js

--- 3dx2v3.js
+++ 3dx2v3.js
@@ -170,7 +170,8 @@ function renderObjects(){		//draws the 3d objects from their coordinates and cam
 	
 	
 	//RENDERING RIGHT WORLD 2D COORDINATE POSITIONS
-	rightCoords = coordinates.map(o => ({x: o.x + eyeDif, y: o.y, z: o.z}) )
+	//rightCoords = coordinates.map(o => ({x: o.x + eyeDif, y: o.y, z: o.z}) )
+	cam.x += eyeDif
 	
 	transformedCoords = rightCoords.map(worldTransate(-cam.x, - cam.y, -cam.z)).map( zWorldRotate(radFromDeg(cam.yaw))).map(xWorldRotate(radFromDeg(cam.pitch))).map(worldTransate(cam.x,cam.y,cam.z))
 	
@@ -181,7 +182,8 @@ function renderObjects(){		//draws the 3d objects from their coordinates and cam
 	drawPoints(canvasCoordinates, 1)
 
 	//RENDERING LEFT WORLD 2D COORDINATE POSITIONS
-	leftCoords = coordinates.map(o => ({x: o.x - eyeDif, y: o.y, z: o.z}) )
+	cam.x -= 2 * eyeDif
+	//leftCoords = coordinates.map(o => ({x: o.x - eyeDif, y: o.y, z: o.z}) )
 	
 	transformedCoords = leftCoords.map(worldTransate(-cam.x, - cam.y, -cam.z)).map( zWorldRotate(radFromDeg(cam.yaw))).map(xWorldRotate(radFromDeg(cam.pitch))).map(worldTransate(cam.x,cam.y,cam.z))
 	
@@ -191,6 +193,7 @@ function renderObjects(){		//draws the 3d objects from their coordinates and cam
 	
 	drawPoints(canvasCoordinates, 2)
 	
+	cam.x += eyeDif
 	
 }
 
